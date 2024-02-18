58becfb72bf0135627e99d95714c3b2dc31de375
ultrabig <161245554+ultrabig@users.noreply.github.com>
2024-02-19 06:13:44 +0900

    parse DBUS_SESSION_BUS_ADDRESS

--- WslNotifyd/Program.cs
+++ WslNotifyd/Program.cs
@@ -1,22 +1,45 @@
 ﻿using System.Diagnostics;
 using Microsoft.Extensions.Hosting;
+using WslNotifyd.DBus;
 using WslNotifyd.Extensions;
 using WslNotifyd.Libc;
 
 internal class Program
 {
+    private static string GetSessionBusPath(string uid)
+    {
+        var busAddress = Environment.GetEnvironmentVariable("DBUS_SESSION_BUS_ADDRESS");
+        if (busAddress == null)
+        {
+            throw new Exception("DBUS_SESSION_BUS_ADDRESS is null");
+        }
+        var entries = AddressEntry.ParseEntries(busAddress);
+        if (entries.Length == 0)
+        {
+            throw new Exception("invalid DBUS_SESSION_BUS_ADDRESS");
+        }
+        var entry = entries[0];
+        var endpoints = entry.ResolveAsync().Result;
+        if (endpoints.Length == 0)
+        {
+            throw new Exception("cannot resolve addresses");
+        }
+        return endpoints[0];
+    }
+
     private static void Main(string[] args)
     {
         var port = "12345";
         var host = "127.0.0.1";
         var uid = Libc.getuid().ToString();
+        var target = GetSessionBusPath(uid);
         var builder = Host.CreateApplicationBuilder(args);
         builder.Services.AddProcessService(new ProcessStartInfo("socat")
         {
             UseShellExecute = false,
             ArgumentList = {
                 $"TCP-LISTEN:{port},reuseaddr,bind={host}",
-                $"UNIX-CLIENT:/run/user/{uid}/bus",
+                target,
             },
             RedirectStandardInput = true,
             RedirectStandardOutput = true,
